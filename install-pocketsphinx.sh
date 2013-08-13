707ba701e9a6538b26ba02b4dd6f45ff9fb3546d
shenzhun <shenzhunallen@gmail.com>
2013-08-14 01:09:44 +0800

    script to install pocketsphinx

---
+++ install-pocketsphinx.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+sudo apt-get install bison gstreamer0.10-pocketsphinx libpocketsphinx-dev libpocketsphinx1 libpython-dbg libpython2.7-dbg libsphinxbase1 pocketsphinx-hmm-en-hub4wsj pocketsphinx-hmm-en-tidigits pocketsphinx-hmm-tidigits pocketsphinx-hmm-wsj1 pocketsphinx-hmm-zh-tdt pocketsphinx-lm-en-hub4 pocketsphinx-lm-wsj pocketsphinx-lm-zh-hans-gigatdt pocketsphinx-lm-zh-hant-gigatdt pocketsphinx-utils python-dbg python-pocketsphinx python-pocketsphinx-dbg python-sphinxbase python2.7-dbg
