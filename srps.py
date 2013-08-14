7986c982dae74e1df850ab4212a1a30807e18316
shenzhun <shenzhunallen@gmail.com>
2013-08-14 22:31:42 +0800

    add test files

--- srps.py
+++ srps.py
@@ -23,9 +23,19 @@ def decodeSpeech(hmmd,lmdir,dictp,wavfile):
 
 
 if __name__ == "__main__":
-        hmdir = ""
-        lmd = ""
-        dictd = ""
-        wavfile = ""
-        recognised = decodeSpeech(hmdir,lmd,dictd,wavfile)
-        print recognised
+        hmdir = "/usr/share/pocketsphinx/model/hmm/wsj1/"
+        lmd = "/usr/share/pocketsphinx/model/lm/wsj/wlist5o.3e-7.vp.tg.lm.DMP"
+        dictd = "/usr/share/pocketsphinx/model/lm/wsj/wlist5o.dic"
+        filelist = sorted(os.listdir("./test/"))
+	print filelist
+	recognisedSent = list()
+	originalSent = list()
+	for i in range(0, len(filelist), 2):
+            recognised = decodeSpeech(hmdir,lmd,dictd,filelist[i+1])
+	    print 'Comparing recognised sentence to origianl one'
+            print recognised.lower()
+	    recognisedSent.append(recognised)
+	    f = file(filelist[i], 'rb')
+	    print f.read()[8:-2]
+	    f.seek(0)
+	    f.close()
