ae6eb834e585b5f9f4d4e93df8a0bdb716eedbcb
shenzhun <shenzhunallen@gmail.com>
2013-09-22 22:35:08 +0800

    create bible sentence withou punct

--- bible_lmtool.py
+++ bible_lmtool.py
@@ -3,9 +3,12 @@
 import nltk
 
 sents = nltk.corpus.gutenberg.sents('bible-kjv.txt')
-for i in sents:
-	for j in i:
-		if j.isalpha() == False:
-			i.remove(i.index(j))
-		print i
+with open('bible_corpus.txt', 'a') as bc:
+	for sent in sents:
+		sent_no_punct = []
+		for word in sent:
+			if word.isalpha():
+				sent_no_punct.append(word)
+		bc.write(' '.join(sent_no_punct) + '\n')
+
 
